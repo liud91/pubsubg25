f98d0d7ee7a843f065c30861ffafb4e383fca419
mootgoat <dm41is@protonmail.ch>
2019-03-28 13:03:04 -0400

    adding config reader

---
+++ G25/projectBaseCode/src/reader/ConfigReader.java
@@ -0,0 +1,30 @@
+package reader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConfigReader {
+    
+    public ConfigReader(String fileName) {
+        super(fileName);
+    }
+    
+    public List<List<String>> getData() {
+        List<List<String>> instructionList = new ArrayList<>();
+        try {
+        	BufferedReader br = new BufferedReader(new FileReader(new File(fileName)));
+			while(br.ready()) {
+				List<String> instructionLine = br.readLine().split(" ");
+				instructionList.add(instructionLine);
+			}
+	        br.close();
+		} catch (IOException e) {
+			instructionList = null;
+		}
+
+        return instructionList;
+}
