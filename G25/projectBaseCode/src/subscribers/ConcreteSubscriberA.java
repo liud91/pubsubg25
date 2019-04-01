265c74a0bfa2357179b809c9dfad9af7b628bc0e
dliu375 <dliu375@uwo.ca>
2019-03-31 21:05:47 -0400

    fixes

--- G25/projectBaseCode/src/subscribers/ConcreteSubscriberA.java
+++ G25/projectBaseCode/src/subscribers/ConcreteSubscriberA.java
@@ -31,7 +31,7 @@ class ConcreteSubscriberA extends AbstractSubscriber {
 	 */
 	@Override
 	public void alert(AbstractEvent event, String channelName) {
-		System.out.println("Subscriber " + this + " handling event ::" + event + ":: published on channel " + channelName);
+		System.out.println("\nSubscriber " + this + " handling event ::" + event + ":: published on channel " + channelName + "\n");
 		state.handleEvent(event, channelName);
 	}
 
